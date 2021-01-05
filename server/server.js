30c128d056108a9fb7e931bf6225ec89aa26fdaa
Darwin <darwincheese@gmail.com>
2021-01-06 00:17:23 +0100

    server cors fixed

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const history = require('connect-history-api-fallback');
+const cors = require('cors');
 const path = require('path');
 
 require('dotenv').config()
@@ -10,6 +11,7 @@ const port = process.env.PORT || 8080;
 
 // Serve up built vue app, at the main root
 const staticFileMiddleware = express.static(path.join('dist'));
+app.use(cors());
 app.use(staticFileMiddleware);
 app.use(history({
   disableDotRule: true,
@@ -20,6 +22,10 @@ app.use(staticFileMiddleware);
 
 // Make all public assets available 
 app.use('/public', express.static('public'))
+
+app.get('/ewo', (req, res, next) => {
+    res.json({msg: 'Hello World!'})
+  })
 
 // App has started
 app.listen(port, () => 
